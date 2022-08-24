303642795b3b08bfa40ffc2e8b6da6683cb5f76e
Otto Krautmann <krautmann.otto@gmail.com>
2022-08-24 09:57:30 -0300

    hicimos que se cargue la tarewa a la lista

--- Desktop/aaaa Otto/Rolling/20 listatareas/src/components/Formulario.js
+++ Desktop/aaaa Otto/Rolling/20 listatareas/src/components/Formulario.js
@@ -24,7 +24,7 @@ const Formulario = () => {
           <Form.Control 
           type="text" 
           placeholder="Ingrese una tarea" 
-          onChange={(e)=>setTarea(e.target.value.trim())}
+          onChange={(e)=>setTarea(e.target.value.trimStart())}
           value={tarea}
            />
           <Button variant="primary" type="submit">
--- Desktop/aaaa Otto/Rolling/20 listatareas/src/components/ItemTarea.js
+++ Desktop/aaaa Otto/Rolling/20 listatareas/src/components/ItemTarea.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ListGroup from "react-bootstrap/ListGroup";
 import Button from 'react-bootstrap/Button';
 
-const ItemTarea = () => {
+const ItemTarea = (props) => {
     return (
             <ListGroup.Item className='d-flex justify-content-between'>
-                Tarea 1
+                {props.tareaCargada}
                 <Button variant='danger'>
                     Borrar
                 </Button>
