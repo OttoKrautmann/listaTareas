828565ff122d5af525b7ac0cdc4d6020b4b2fa3b
Otto Krautmann <krautmann.otto@gmail.com>
2022-09-03 13:29:21 -0300

    termine de configurar el navegador

--- Desktop/aaaa Otto/Rolling/22 cafeteria/src/components/common/Navegador.js
+++ Desktop/aaaa Otto/Rolling/22 cafeteria/src/components/common/Navegador.js
@@ -2,18 +2,20 @@ import React from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
+import {NavLink, Link} from "react-router-dom"
 
 const Navegador = () => {
   return (
     <div>
-      <Navbar expand="lg" bg="dark" variant="dark" className="py-3">
+      <Navbar expand="lg" bg="danger" variant="dark" className="py-3">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">Cafeteria</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Inicio</Nav.Link>
-              <Nav.Link href="#link">Administrador</Nav.Link>
+              <NavLink end to="/" className="nav-item nav-link">Inicio</NavLink>
+              <NavLink end to="/administrador" className="nav-item nav-link">Administrar</NavLink>
+              <NavLink end to="/crearProducto" className="nav-item nav-link">Crear producto</NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
